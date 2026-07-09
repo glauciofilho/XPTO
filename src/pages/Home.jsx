3ff0aec634d5a75371ff2990f435d257cf019094
Gláucio Filho <101465988+glauciofilho@users.noreply.github.com>
2026-07-09 10:11:20 -0300

    Update footer to include author name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ export default function Home({ isDarkMode }) {
       <footer className="py-10 px-16 border-t border-outline dark:border-slate-900 bg-white dark:bg-slate-950 transition-colors duration-200">
         <div className="max-w-6xl mx-auto flex items-center justify-between">
           <p className="text-label-md text-tertiary dark:text-slate-400">
-            {t('hero.tag')}
+            Gláucio Alves Pereira Filho - {t('hero.tag')}
           </p>
           <p className="text-label-md text-tertiary dark:text-slate-400">
             2026
